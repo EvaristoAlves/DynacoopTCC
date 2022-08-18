bd1ee2c4a8f2e03ac4016232734ffd1f0992f1f6
Evaristo Alves <evaristocarvalho34@gmail.com>
2022-08-18 18:40:25 -0300

    AtualizandoApiCep

---
+++ DynacoopTCC/ConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynacoopTCC
+{
+    public class ConnectionFactory
+    {
+        public static IOrganizationService GetService()
+        {
+            string connectionString =
+                "AuthType=OAuth;" +
+                "Username=TCCDynacoop@tccdyp.onmicrosoft.com;" +
+                "Password=tccdyp@123;" +
+                "Url=https://dynamics01.crm2.dynamics.com/;" +
+                "AppId=19f8767c-2706-464b-89ea-259d2d7db63e;" +
+                "RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;";
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            return crmServiceClient.OrganizationWebProxyClient;
+        }
+    }
+}
